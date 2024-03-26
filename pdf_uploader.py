07a5be3d317603eac871c563ed3af0eeb537f079
Trung Nguyễn <nguyenchitrungds@gmail.com>
2024-03-26 23:42:26 +0700

    Update pdf_uploader.py

--- pdf_uploader.py
+++ pdf_uploader.py
@@ -11,7 +11,17 @@ import cv2
 import os
 import tempfile
 import re
-pytesseract.pytesseract.tesseract_cmd = r"/opt/homebrew/Cellar/tesseract/5.3.4_1/bin/tesseract"
+pytesseract.pytesseract.tesseract_cmd = None
+
+# search for tesseract binary in path
+@st.cache_resource
+def find_tesseract_binary() -> str:
+    return shutil.which("tesseract")
+
+# set tesseract binary path
+pytesseract.pytesseract.tesseract_cmd = find_tesseract_binary()
+if not pytesseract.pytesseract.tesseract_cmd:
+    st.error("Tesseract binary not found in PATH. Please install Tesseract.")
 
 
 def upload_pdf_file():
